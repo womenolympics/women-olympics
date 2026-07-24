b4eae913dbcafc4c6a6a8a565b3cea040d278472
womenolympics <bureau.wolympics@gmail.com>
2026-07-25 01:37:57 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const menuToggle = document.getElementById('menuToggle');
+const mainNav = document.getElementById('mainNav');
+
+if (menuToggle && mainNav) {
+  menuToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('open');
+  });
+
+  mainNav.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => mainNav.classList.remove('open'));
+  });
+}
